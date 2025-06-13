27be681e6f4753d0172f255b36f36cbc89a4dd47
jswallah <jswallah1@gmail.com>
2025-06-13 21:47:29 +0530

    fix again

--- src/app/darshan-schedule/page.tsx
+++ src/app/darshan-schedule/page.tsx
@@ -112,7 +112,7 @@ export default function DarshanSchedulePage() {
                     </div>
                   ))}
                 </div>
-              </div>
+              </div>    
             ))}
           </div>
         </div>
