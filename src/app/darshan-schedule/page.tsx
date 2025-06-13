6832c81145e49e8faf2b5833e442927f0036fb9e
jswallah <jswallah1@gmail.com>
2025-06-13 21:30:14 +0530

    fix deploye essue

--- src/app/darshan-schedule/page.tsx
+++ src/app/darshan-schedule/page.tsx
@@ -113,7 +113,7 @@ export default function DarshanSchedulePage() {
                   ))}
                 </div>
               </div>
-            ))} 
+            ))}
           </div>
         </div>
       </main>
